1287c3ec9dbfbdb99cf69430c6549dd3625c66c4
soronpo <soronpo@tx.technion.ac.il>
2021-07-20 14:18:59 +0300

    further minimization

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,3 +1,3 @@
 object Test:
-  val x  = summon[Lie.TC[Lie[7], Lie[9]]]
-  val fails = summon[x.Out =:= Samurai.Check[7, 9]]
+  val x  = summon[Lie.TC[Lie[7]]]
+  val fails = summon[x.Out =:= Samurai.Check[7]]
