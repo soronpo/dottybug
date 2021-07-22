12d3c5b9a6abb45eb2b46b43b7258d65e5bcb855
soronpo <soronpo@tx.technion.ac.il>
2021-07-22 17:15:25 +0300

    Minimized project for opaque type argument widening

--- src/main/scala/DFBool.scala
+++
@@ -1,5 +0,0 @@
-trait HasToken
-
-opaque type DFBool <: DFType with HasToken = DFType with HasToken
-
-def bar(v: DFVal): Unit = v.as[DFBool]
--- src/main/scala/Width.scala
+++
@@ -1,6 +0,0 @@
-import scala.quoted.*
-
-trait Width
-object Width:
-  inline given Width = ${ getWidthMacro }
-  def getWidthMacro(using Quotes): Expr[Width] = '{ new Width {} }
