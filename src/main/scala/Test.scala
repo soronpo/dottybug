16a89c6cc06d698c1708fcf573198079ce1ec1da
soronpo <oron.port@cornell.edu>
2021-09-03 12:28:41 -0400

    minimizing incremental compilation opaque error

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,3 +1,4 @@
 val myOpaque: MyOpaque[8] = ???
 val container = myOpaque.getContainer
+//after successfull compilation, uncomment the following line to get the error
 //val shouldWork = summon[container.type <:< Container[8]]
