2e2506c6700faa457e0132052235fb78c20de86d
oronpo <oron.port@cornell.edu>
2022-08-22 15:25:07 -0400

    Minimize wrong run crash

--- src/main/scala/dfhdl/core/DFType.scala
+++
@@ -1,8 +0,0 @@
-package dfhdl.core
-import scala.annotation.precise
-
-object DFType:
-  object Ops:
-    trait Floozy[V]
-    extension[T] (t: T) def tester[@precise V](tokenValue: V): Floozy[V] = ???
-
--- src/main/scala/dfhdl/hdl.scala
+++ src/main/scala/dfhdl/hdl.scala
@@ -1,4 +1,15 @@
 package dfhdl
+
+object core:
+
+  import scala.annotation.precise
+
+  object DFType:
+    object Ops:
+      trait Floozy[V]
+
+      extension[T] (t: T) def tester[@precise V](tokenValue: V): Floozy[V] = ???
+
 object hdl:
   export core.DFType.Ops.*
 
