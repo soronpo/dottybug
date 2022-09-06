960dde2821fcd0ea385be2becf0edf56e23fc8aa
oronpo <oron.port@cornell.edu>
2022-09-06 19:57:15 -0400

    precisebug

--- src/test/scala/precise-test.scala
+++ src/test/scala/precise-test.scala
@@ -6,6 +6,5 @@ object ShowType:
   given [T]: ShowType[T] = ???
 
 object check:
-//  val v: DFVector[DFBool, Tuple1[4]] = ???
   def box[A](a: A)(using ShowType[A]): Unit = ???
-  val b1 = box(Boolean XX 4 XX 4)
+  val b1 = box(Boolean XX 4)
