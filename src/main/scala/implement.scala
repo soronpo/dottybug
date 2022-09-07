a67d6dcb25b5d9a2294c5791a5341a29eedc231b
oronpo <oron.port@cornell.edu>
2022-09-06 20:00:35 -0400

    precisebug

--- src/main/scala/implement.scala
+++ src/main/scala/implement.scala
@@ -16,18 +16,3 @@ object preciseLib:
   type DFVector[+T <: DFTypeAny, +D <: NonEmptyTuple] =
     DFType[IRDFVector, Args2[T @uncheckedVariance, D @uncheckedVariance]]
 
-  trait TC[@precise -T]:
-    type Type <: DFTypeAny
-    def apply(t: T): Type
-  object TC:
-    transparent inline given ofDFType[T <: DFTypeAny]: TC[T] = new TC[T]:
-      type Type = T
-      def apply(t: T): Type = t
-    transparent inline given ofBooleanCompanion: TC[Boolean.type] = new TC[Boolean.type] :
-      type Type = DFBool
-      def apply(t: Boolean.type): Type = ???
-
-  extension [T, @precise D <: Int](t: T)(using tc: TC[T])
-    def XX(
-      cellDim: D
-    ): DFVector[tc.Type, Tuple1[D]] = ???
--- src/test/scala/precise-test.scala
+++ src/test/scala/precise-test.scala
@@ -1,5 +1,23 @@
 import preciseLib.*
 
+import scala.annotation.precise
+
+trait TC[@precise -T]:
+  type Type <: DFTypeAny
+  def apply(t: T): Type
+object TC:
+  transparent inline given ofDFType[T <: DFTypeAny]: TC[T] = new TC[T]:
+    type Type = T
+    def apply(t: T): Type = t
+  transparent inline given ofBooleanCompanion: TC[Boolean.type] = new TC[Boolean.type] :
+    type Type = DFBool
+    def apply(t: Boolean.type): Type = ???
+
+extension [T, @precise D <: Int](t: T)(using tc: TC[T])
+  def XX(
+    cellDim: D
+  ): DFVector[tc.Type, Tuple1[D]] = ???
+
 trait ShowType[T]
 object ShowType:
   given [T]: ShowType[T] = ???
